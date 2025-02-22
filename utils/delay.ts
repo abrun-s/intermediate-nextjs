8be22d22c26758824a9a6d4955ef938bd4ada007
adam brunson <adam.brunson@yahoo.com>
2025-02-22 07:57:08 -0800

    Server-side Data Fetching && delay

--- utils/delay.ts
+++ utils/delay.ts
@@ -1,3 +1,4 @@
+// delay the retveil to see the action
 export const delay = (max = 2500) => {
   const randomDelayTime = Math.floor(Math.random() * (max - 500 + 1))
   return new Promise((resolve) => setTimeout(resolve, randomDelayTime))
