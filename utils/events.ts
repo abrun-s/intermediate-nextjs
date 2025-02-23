76b07726b7677bcc9cd32eb571bb5e8300d4f2e4
adam brunson <adam.brunson@yahoo.com>
2025-02-23 07:20:07 -0800

    fethching Events & Rsvps

---
+++ utils/events.ts
@@ -0,0 +1,26 @@
+import 'server-only'
+import { delay } from './delay'
+import { asc, eq } from 'drizzle-orm'
+import { events } from '@/db/schema'
+import { db } from '@/db/db'
+
+export const getEventsForDashboard = async (userId:string) => {
+  await delay()
+
+  const data = await db.query.events.findMany({
+    where: eq(events.createdById, userId),
+    columns: {
+      id: true,
+      name: true,
+      startOn: true,
+      status: true,
+    },
+    with: {
+      rsvps: true,
+    },
+    limit: 5,
+    orderBy: [asc(events.startOn)],
+  })
+
+  return data ?? []
+}
---
+++ utils/rsvps.ts
@@ -0,0 +1,30 @@
+import 'server-only'
+import { delay } from './delay'
+import { db } from '@/db/db'
+import { desc, eq, inArray } from 'drizzle-orm'
+import { attendees, events, rsvps } from '@/db/schema'
+
+export const getRsvpsForDashboard = async (userId:string) => {
+  await delay
+
+  const userEvents = await db.query.events.findMany({
+    where: eq(events.createdById, userId),
+    columns: {
+      id: true,
+    },
+  })
+
+  const userEventIds = userEvents.map((event) => event.id)
+  if (!userEventIds.length) return []
+
+  const data = await db
+    .selectDistinct()
+    .from(attendees)
+    .where(inArray(rsvps.eventId, userEventIds))
+    .leftJoin(rsvps, eq(attendees.id, rsvps.attendeeId))
+    .leftJoin(events, eq(rsvps.eventId, events.id))
+    .orderBy(desc(rsvps.createdAt))
+    .execute()
+
+  return data
+}
